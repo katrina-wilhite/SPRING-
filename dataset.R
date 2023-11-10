a3039ef8d42028c0b535255106142521b8e58f1d
katrina-wilhite <62529848+katrina-wilhite@users.noreply.github.com>
2023-11-10 16:36:41 -0500

    Created dataset to include only relevant columns

--- dataset.R
+++ dataset.R
@@ -1,7 +1,18 @@
-str(`SPRING data`)
-df <- `SPRING data`
-View(df[,1:50])
-View(df[,51:100])
-View(df[,101:150])
-View(df[,151:200])
-View(df[,201:ncol(df)])
+SPRING <- `SPRING data`
+View(SPRING[,1:50])
+View(SPRING[,51:100])
+View(SPRING[,101:150])
+View(SPRING[,151:200])
+View(SPRING[,201:ncol(SPRING)])
+
+#Open appropriate libraries 
+library("dplyr")
+
+#Select relevant columns 
+df <- SPRING %>% 
+  select(c(record_id:redcap_event_name, psqi_bedtime:psqi_enthusiasm, steps_day:sed_min, wear_min:weardays, cr_prepregwt_kgs, gawks_v1:af_gadays_v3, livebirths, meds, randomization, af_height_feet:af_height_inches))
+
+
+
+
+
