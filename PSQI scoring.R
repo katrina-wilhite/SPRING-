80541f98c2ce54ea9688cb1b49c391fed74cdb4a
katrina-wilhite <62529848+katrina-wilhite@users.noreply.github.com>
2023-11-10 16:36:05 -0500

    Outlined code to score PSQI questionnaire

---
+++ PSQI scoring.R
@@ -0,0 +1,46 @@
+#Subjective sleep quality: Question 6 - psqi_quality; assign component score
+
+
+#Sleep latency: Questions 2 and 5a - psqi_fallasleep & psqi_30min; assign component subscore
+
+
+##Sleep latency; sum question 2 and 5a
+
+
+##Sleep latency; assign component score 
+
+
+#Sleep duration: Question 4 - psqi_sleep; assign component score 
+
+
+#Habitual sleep efficiency: calculate number of hours spent in bed (question 3 - question 1) - psqi_bedtime & psqi_getup
+
+
+#Habitual sleep efficiency: (psqi_sleep/hours spent in bed) X 100 = % 
+
+
+#Habitual sleep efficiency: assign component score 
+
+
+#Sleep disturbances: questions 5b-5j - psqi_middle:psqi_other; assign component subscores for each 
+
+
+#Sleep disturbances: Sum scores of psqi_middle:psqi_other 
+
+
+#Sleep disturbances: assign component score 
+
+
+#Use of sleep medication: Question 7 - psqi_meds; assign component score 
+
+
+#Daytime dysfunction: Questions 8 and 9; psqi_stayawake & psqi_enthusiasm; assign component subscores for each 
+
+
+#Daytime dysfunction: sum scores of psqi_stayawake & psqi_enthusiasm 
+
+
+#Daytime dysfunction: assign component score 
+
+
+#Global PSQI Score: sum all 7 components together 
