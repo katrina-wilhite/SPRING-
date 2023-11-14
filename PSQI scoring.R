11a2db86746aaa2e83f0a3c4c140f8a451f742f5
Katrina Wilhite <katrina.wilhite@gmail.com>
2023-11-14 14:14:04 -0500

    Scored sleep latency

--- PSQI scoring.R
+++ PSQI scoring.R
@@ -11,8 +11,9 @@ df$quality_scored <- recode(df$quality_scored,
 
 #Sleep latency: Questions 2 and 5a - psqi_fallasleep & psqi_30min; assign component subscore
 df$fallasleep_scored <- df$psqi_fallasleep
-df$fallasleep_scored <- recode(df$fallasleep_scored)
-
+breakpoints_latency1 <- c(-Inf, 15, 30, 60, Inf)
+df$fallasleep_scored <- cut(df$fallasleep_scored, breaks = breakpoints_latency1, labels = c(0,1,2,3), include.lowest = TRUE)
+df$fallasleep_scored <- as.numeric(df$fallasleep_scored)
 
 df$psqi30_scored <- df$psqi_30min
 df$psqi30_scored <- recode(df$psqi30_scored,
@@ -22,10 +23,13 @@ df$psqi30_scored <- recode(df$psqi30_scored,
                            "Three or more times a week" = 3)
 
 ##Sleep latency; sum question 2 and 5a
-
+df <- df %>% 
+  mutate(latency_sum = (fallasleep_scored + psqi30_scored))
 
 ##Sleep latency; assign component score 
-
+df$latency_scored <- df$latency_sum
+breakpoints_latency2 <- c(-Inf, 0, 2, 4, 6)
+df$latency_scored <- cut(df$latency_scored, breaks = breakpoints_latency2, labels = c(0,1,2,3), include.lowest = TRUE)
 
 #Sleep duration: Question 4 - psqi_sleep; assign component score 
 df$sleep_scored <- df$psqi_sleep
