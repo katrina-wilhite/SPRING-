9ef07bb963bf58b6bb705b8e50dc0cdd67de2aaf
katrina-wilhite <62529848+katrina-wilhite@users.noreply.github.com>
2023-11-17 15:53:18 -0500

    saved results

--- analysis.R
+++ analysis.R
@@ -13,7 +13,7 @@ global_model <- lmer(global_psqi_score ~ randomization +
 global_reduced <- lmer(global_psqi_score ~  + 
        (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
-anova(global_reduced,global_model)
+global_results <- anova(global_reduced,global_model)
 
 #Repeat for sleep quality 
 quality_model <- lmer(quality_scored ~ randomization + 
@@ -22,7 +22,7 @@ quality_model <- lmer(quality_scored ~ randomization +
 quality_reduced <- lmer(quality_scored ~  + 
                          (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
-anova(quality_reduced,quality_model)
+quality_results <-anova(quality_reduced,quality_model)
 
 #Repeat for sleep latency 
 latency_model <- lmer(latency_scored ~ randomization + 
@@ -31,7 +31,7 @@ latency_model <- lmer(latency_scored ~ randomization +
 latency_reduced <- lmer(latency_scored ~  + 
                           (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
-anova(latency_reduced,latency_model)
+latency_results <-anova(latency_reduced,latency_model)
 
 #Repeat for sleep duration 
 duration_model <- lmer(psqi_sleep ~ randomization + 
@@ -40,7 +40,7 @@ duration_model <- lmer(psqi_sleep ~ randomization +
 duration_reduced <- lmer(psqi_sleep ~  + 
                           (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
-anova(duration_reduced,duration_model)
+duration_results <-anova(duration_reduced,duration_model)
 
 #Repeat for sleep efficiency 
 efficiency_model <- lmer(efficiency ~ randomization + 
@@ -49,7 +49,7 @@ efficiency_model <- lmer(efficiency ~ randomization +
 efficiency_reduced <- lmer(efficiency ~  + 
                            (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
-anova(efficiency_reduced,efficiency_model)
+efficiency_results <-anova(efficiency_reduced,efficiency_model)
 
 #Repeat for sleep disturbances 
 disturbances_model <- lmer(disturbances_scored ~ randomization + 
@@ -58,7 +58,7 @@ disturbances_model <- lmer(disturbances_scored ~ randomization +
 disturbances_reduced <- lmer(disturbances_scored ~  + 
                              (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
-anova(disturbances_reduced,disturbances_model)
+disturbances_results <-anova(disturbances_reduced,disturbances_model)
 
 #Repeat for daytime dysfunction 
 daytime_dysfunction_model <- lmer(daytime_dysfunction_scored ~ randomization + 
@@ -67,4 +67,4 @@ daytime_dysfunction_model <- lmer(daytime_dysfunction_scored ~ randomization +
 daytime_dysfunction_reduced <- lmer(daytime_dysfunction_scored ~  + 
                                (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
-anova(daytime_dysfunction_reduced,daytime_dysfunction_model)
+daytime_dysfunction_results <-anova(daytime_dysfunction_reduced,daytime_dysfunction_model)
