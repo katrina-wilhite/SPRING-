b078c418faacfdabee56021a7a17929ebe55b4ac
katrina-wilhite <62529848+katrina-wilhite@users.noreply.github.com>
2023-11-21 15:43:41 -0500

    Added visit as covariate to all sleep components

--- analysis.R
+++ analysis.R
@@ -16,55 +16,55 @@ global_reduced <- lmer(global_psqi_score ~  + global_psqi_score_baseline + redca
 global_results <- anova(global_reduced,global_model)
 
 #Repeat for sleep quality 
-quality_model <- lmer(quality_scored ~ randomization + quality_scored_baseline +
+quality_model <- lmer(quality_scored ~ randomization + quality_scored_baseline + redcap_event_name +
                        (1 | record_id), data = df_complete, REML = FALSE)
 ##Build reduced model
-quality_reduced <- lmer(quality_scored ~  + quality_scored_baseline +
+quality_reduced <- lmer(quality_scored ~  + quality_scored_baseline + redcap_event_name +
                          (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
 quality_results <-anova(quality_reduced,quality_model)
 
 #Repeat for sleep latency 
-latency_model <- lmer(latency_scored ~ randomization + latency_scored_baseline +
+latency_model <- lmer(latency_scored ~ randomization + latency_scored_baseline + redcap_event_name +
                         (1 | record_id), data = df_complete, REML = FALSE)
 ##Build reduced model
-latency_reduced <- lmer(latency_scored ~  + latency_scored_baseline +
+latency_reduced <- lmer(latency_scored ~  + latency_scored_baseline + redcap_event_name +
                           (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
 latency_results <-anova(latency_reduced,latency_model)
 
 #Repeat for sleep duration 
-duration_model <- lmer(psqi_sleep ~ randomization + psqi_sleep_baseline +
+duration_model <- lmer(psqi_sleep ~ randomization + psqi_sleep_baseline + redcap_event_name +
                         (1 | record_id), data = df_complete, REML = FALSE)
 ##Build reduced model
-duration_reduced <- lmer(psqi_sleep ~  + psqi_sleep_baseline +
+duration_reduced <- lmer(psqi_sleep ~  + psqi_sleep_baseline + redcap_event_name +
                           (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
 duration_results <-anova(duration_reduced,duration_model)
 
 #Repeat for sleep efficiency 
-efficiency_model <- lmer(efficiency ~ randomization + efficiency_baseline +
+efficiency_model <- lmer(efficiency ~ randomization + efficiency_baseline + redcap_event_name +
                          (1 | record_id), data = df_complete, REML = FALSE)
 ##Build reduced model
-efficiency_reduced <- lmer(efficiency ~  + efficiency_baseline +
+efficiency_reduced <- lmer(efficiency ~  + efficiency_baseline + redcap_event_name +
                            (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
 efficiency_results <-anova(efficiency_reduced,efficiency_model)
 
 #Repeat for sleep disturbances 
-disturbances_model <- lmer(disturbances_scored ~ randomization + disturbances_scored_baseline +
+disturbances_model <- lmer(disturbances_scored ~ randomization + disturbances_scored_baseline + redcap_event_name +
                            (1 | record_id), data = df_complete, REML = FALSE)
 ##Build reduced model
-disturbances_reduced <- lmer(disturbances_scored ~  + disturbances_scored_baseline +
+disturbances_reduced <- lmer(disturbances_scored ~  + disturbances_scored_baseline + redcap_event_name +
                              (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
 disturbances_results <-anova(disturbances_reduced,disturbances_model)
 
 #Repeat for daytime dysfunction 
-daytime_dysfunction_model <- lmer(daytime_dysfunction_scored ~ randomization + daytime_dysfunction_scored_baseline +
+daytime_dysfunction_model <- lmer(daytime_dysfunction_scored ~ randomization + daytime_dysfunction_scored_baseline + redcap_event_name +
                              (1 | record_id), data = df_complete, REML = FALSE)
 ##Build reduced model
-daytime_dysfunction_reduced <- lmer(daytime_dysfunction_scored ~  + daytime_dysfunction_scored_baseline +
+daytime_dysfunction_reduced <- lmer(daytime_dysfunction_scored ~  + daytime_dysfunction_scored_baseline + redcap_event_name +
                                (1 | record_id), data = df_complete, REML = FALSE)
 ##Compare models 
 daytime_dysfunction_results <-anova(daytime_dysfunction_reduced,daytime_dysfunction_model)
